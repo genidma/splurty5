f2f37061a252aad3b23f57fc0c6bb6e029c2405c
Adeel Khan <adeelnkhan@gmail.com>
2015-04-06 03:41:31 +0000

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Splurty5::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurty5::Application.config.secret_key_base = 'ba6098a64d9fb09e8152c6d1c352a703027193d00a1695fa0e62df0a4630a7213b2c9f7cf7172dd5eb2df66b57e844456864020e900dac869b19973dfe13db38'
